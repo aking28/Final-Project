78543bbbf62bd6f395b8e94d055a72f3e3e16597
aking28 <aking28@me.com>
2017-12-03 21:26:37 -0500

    Adding app

---
+++ app/main.js
@@ -0,0 +1,8 @@
+console.log('This is main.js')
+
+let content = document.getElementById('content')
+
+let title = document.createElement('h1')
+title.append('Final Project')
+
+content.append(title)
